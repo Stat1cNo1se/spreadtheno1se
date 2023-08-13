acc6a62dd2d529a40c24a56004b3c8e5babdce02
Stat1cNo1se <142123884+Stat1cNo1se@users.noreply.github.com>
2023-08-13 14:38:31 +0200

    Update NOISE.py

--- NOISE.py
+++ NOISE.py
@@ -2,19 +2,15 @@ import tkinter as tk
 import random
 from PIL import Image, ImageTk
 
-# Create the main application window
 root = tk.Tk()
-root.attributes("-fullscreen", True)  # Make the window full-screen
+root.attributes("-fullscreen", True)
 
-# Get the screen width and height
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
-# Create a Canvas widget to display the noise
 canvas = tk.Canvas(root, width=screen_width, height=screen_height)
 canvas.pack()
 
-# Generate and update the noise every 200 milliseconds
 def generate_noise():
     noise_image = Image.new("RGB", (screen_width, screen_height))
     pixels = []
@@ -27,9 +23,8 @@ def generate_noise():
     canvas.create_image(0, 0, image=noise_tk, anchor="nw")
     canvas.image = noise_tk
     
-    root.after(200, generate_noise)  # Call generate_noise after 200 milliseconds
+    root.after(200, generate_noise)
 
-# Start generating noise
 generate_noise()
 
 root.mainloop()
